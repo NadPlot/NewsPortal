cb84bb5c3aa80d7867cdae441497013ee7946204
Nadegda <nadin45681@gmail.com>
2022-11-08 13:28:13 +1200

    add Author test

--- news/admin.py
+++ news/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Category, Author, Post, PostCategory, Comment, Subscribes
 
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("user", "user_rating")
+
+
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Post)
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from .models import Author, Post
 
-# Create your tests here.
+
+class AuthorTests(TestCase):
+
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(
+                username='name',
+                email='email@email.ru',
+                password='test123'
+        )
+        
+        def test_create_author(self):
+            author = Author.objects.create(user=self.user)
+            self.assertEqual(author.user.username, 'name')
+
