7d6ebce1f5638f51265d9eb6e4e44fa5c28d1977
Nadegda <nadin45681@gmail.com>
2022-11-10 14:37:57 +1200

    add homepage

--- news/admin.py
+++ news/admin.py
@@ -6,9 +6,13 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ("user", "user_rating")
 
 
-admin.site.register(Author)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "add_time", "author", "post_rating")
+
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
 admin.site.register(Subscribes)
--- news/tests.py
+++ news/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 from django.urls import reverse
 from .models import Author, Post
 
@@ -17,4 +17,17 @@ class AuthorTests(TestCase):
         def test_create_author(self):
             author = Author.objects.create(user=self.user)
             self.assertEqual(author.user.username, 'name')
+            self.assertEqual(author.user.email, 'email@email.ru')
+            self.assertTrue(author.user.is_active)
+
+
+class HomepageTests(SimpleTestCase):
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
 
